856e048da71a2ad4c2d43dc33804809cb2ed9327
Amnay <0xamlou@gmail.com>
2011-03-18 17:50:26 +0000

    Schalke!

--- SGF/Testeur.c
+++ SGF/Testeur.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include "DD.h"
 #include "TOS.h"
+#include "CI.h"
 main()
 {
-	MakeHD("one.hd", 5);
+//	MakeHD("one.hd", 5);
 	ProbeHD("one.hd");
 	char datum[16], rep = 1 ;
-	int NumSect;
+	int N;
+	CI Carte;
+	InitCI(&Carte);SauvegarderCI(&Carte, 0);
+	DisplayData(&Carte);
 	while(rep)
+	{
+		ChargerCI(&Carte, 0);
+		scanf("%d", &rep);
+		if(rep == 1)
+		{
+			
+			scanf("%d", &N);
+			InsererIndiceSecteurCI(&Carte, N);
+			DisplayData(&Carte);
+			
+		}
+		else if(rep == 2)
+		{
+			scanf("%d", &N);
+			OterIndiceSecteurCI(&Carte, N);
+			DisplayData(&Carte);
+		}
+		SauvegarderCI(&Carte, 0);
+	}
+
+
+
+
+/*while(rep)
 	{
 		printf("Vous voulez:\n1.Lire Secteur\n2.Ecrire Secteur\n");
 		scanf("%d", &rep);
@@ -28,10 +56,11 @@ main()
 			DisplayData(datum);
 		}
 	}	
-
+*/
 //	ReadSector(0, datum);
 //	DisplayData(datum);
 //	char String[16] = "Calypsus";
 //	WriteSector(3, String);
 }
+
 
